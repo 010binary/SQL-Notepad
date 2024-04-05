c2f3f341793f78b72531244e3eb52c301c83a68a
010binary <augustine32fg@gmail.com>
2024-04-05 16:53:59 +0100

    custom colums

---
+++ data_manipulation_language/03_custom_colums.sql
@@ -0,0 +1,8 @@
+--AS
+-- This is a special function used to turn queries into a colum nb we can chain this command however we want
+-- usage: SELECT UPPER("Departure") AS "Departure_CAPS" FROM Flight;
+SELECT UPPER("Departure") AS "Departure_CAPS",
+    LOWER("Destination") AS "Destination_uncaps",
+    ("Duration" + 10) AS "POssible_delay_time" FROM Flight;
+
+-- this Forms 3 colums when returning the result of the query
